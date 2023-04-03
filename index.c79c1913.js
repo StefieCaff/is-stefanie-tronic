a4306668ade7a691f44666fd29da7c06b52b654c
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-04-03 03:05:29 +0000

    Deploying to gh-pages from @ StefieCaff/is-stefanie-tronic@10f0656eced96adb98f168dde2d67f224a2d5b7e 🚀

---
+++ index.c79c1913.js
@@ -0,0 +1,2 @@
+({questionBtn:document.querySelector("#question-btn"),stickyBtn:document.querySelector("#sticky-btn"),planeBtn:document.querySelector("#plane-btn"),planePage:document.querySelector("#plane-js"),stickPage:document.querySelector("#sticky-js"),questionPage:document.querySelector("#question-js")}).questionBtn.addEventListener("click",(()=>{planePage.classList.add("hidden"),stickyPage.classList.add("hidden"),questionBtn.classList.add("hidden"),stickyBtn.classList.remove("hidden")}));
+//# sourceMappingURL=index.c79c1913.js.map
