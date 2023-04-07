d68e18d7032afccce402ef2482daf693c5935e50
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-04-06 18:10:13 -0700

    complete questions
    styling too

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 // DOM Refs
 
 const refs = {
-    questionBtn: document.querySelector('#question-btn'),
-    stickyBtn: document.querySelector('#sticky-btn'),
-    planeBtn: document.querySelector('#plane-btn'),
-    planePage: document.querySelector('#plane-js'),
-    stickPage: document.querySelector('#sticky-js'),
-    questionPage: document.querySelector('#question-js')
+    questionBtn: document.querySelector('#question-btn-js'),
+    stickyBtn: document.querySelector('#sticky-btn-js'),
+    planeBtn: document.querySelector('#plane-btn-js'),
+
+    questionBtnWrapper: document.querySelector('.question-js'),
+    stickyBtnWrapper: document.querySelector('.sticky-js'),
+    planeBtnWrapper: document.querySelector('.plane-js'),
+
+    plane: document.querySelector('#plane-js'),
+    questionPage: document.querySelector('#question-js'),
+    stickyPage: document.querySelector('#sticky-js')
 };
 
 // helper functions to add and remove elements from DOM
 
-const navToQuestions = () => {
-    planePage.classList.add('hidden');
-    stickyPage.classList.add('hidden');
-    questionBtn.classList.add('hidden');
-    stickyBtn.classList.remove('hidden');
+const navToQuestionPage = () => {
+    refs.plane.classList.add('hidden');
+    refs.questionBtnWrapper.classList.add('hidden');
+    refs.stickyBtnWrapper.classList.remove('hidden');
+    refs.questionPage.classList.remove('hidden');
+    console.log('test');
 };
 
-// event listeners
+const navToStickyPage = () => {
+    refs.stickyBtnWrapper.classList.add('hidden');
+    refs.questionPage.classList.add('hidden');
+    refs.planeBtnWrapper.classList.remove('hidden');
+    refs.stickyPage.classList.remove('hidden');
+    console.log('test two');
+};
 
-refs.questionBtn.addEventListener('click', navToQuestions);
+const navToPlanePage = () => {
+    refs.planeBtnWrapper.classList.add('hidden');
+    refs.stickyPage.classList.add('hidden');
+    refs.questionBtnWrapper.classList.remove('hidden');
+    refs.plane.classList.remove('hidden');
+    console.log('test three');
+};
 
+// // event listeners
+
+refs.questionBtn.addEventListener('click', navToQuestionPage);
+
+refs.stickyBtn.addEventListener('click', navToStickyPage);
+
+refs.planeBtn.addEventListener('click', navToPlanePage);
 
 
